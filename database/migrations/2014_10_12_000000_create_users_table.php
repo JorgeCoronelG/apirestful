2347dd5ffd6b18dc8c9eca21d38b6eef1449a406
Jorge Coronel González <tprog.jorge.coronel@outlook.com>
2020-09-27 15:15:18 -0500

    Se edita la migración de users Se crea la migración de leagues

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,7 +3,15 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use \App\Models\User;
 
+/**
+ * Class CreateUsersTable
+ *
+ * @author JorgeCoronelG
+ * @version 1.0
+ * Created 27/07/2020
+ */
 class CreateUsersTable extends Migration
 {
     /**
@@ -15,11 +23,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('email', 120)->unique();
             $table->string('password');
-            $table->rememberToken();
+            $table->tinyInteger('role');
+            $table->tinyInteger('verified')->default(User::USUARIO_NO_VERIFICADO);
+            $table->string('verification_token', '40')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
             $table->timestamps();
         });
     }
---
+++ database/migrations/2020_09_26_220348_create_leagues_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * Class CreateLeaguesTable
+ *
+ * @author JorgeCoronelG
+ * @version 1.0
+ * Created 27/09/2020
+ */
+class CreateLeaguesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('leagues', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', '120');
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('leagues');
+    }
+}
