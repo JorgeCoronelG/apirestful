807cda947137157dc974fb390b3d3645eef65469
Jorge Coronel González <tprog.jorge.coronel@outlook.com>
2020-10-05 07:50:54 -0500

    Se agregan las rutas para login y logout Se usa el middleware de auth:api

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,14 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\League\LeagueController;
+use \App\Http\Controllers\User\LoginController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::prefix('v1')->group(function () {
+    Route::post('/login', [LoginController::class, 'login']);
+});
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->prefix('v1/app')->group(function () {
+    Route::apiResource('leagues',LeagueController::class);
+    Route::get('/users/{user}/logout', [LoginController::class, 'logout']);
 });
