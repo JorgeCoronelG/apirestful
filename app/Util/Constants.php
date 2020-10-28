2e5c59021f6c8bed25ad79dfacde7c6796a9552a
Jorge Coronel González <tprog.jorge.coronel@outlook.com>
2020-10-28 14:03:19 -0600

    General * Se crea Constants para aquellas variables de tipo int que se usen en el sistema * Se mueve carpeta de Util a la raíz de App * Se editan mensajes de Messages

---
+++ app/Util/Constants.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Util;
+
+/**
+ * Class Constants
+ *
+ * @author JorgeCoronelG
+ * @version 1.0
+ * @package App\Util
+ * Created 27/10/2020
+ */
+class Constants
+{
+    // Times something is executed
+    const TIMES_TO_RESEND_EMAIL = 5;
+
+    // Sleep
+    const SLEEP_TO_RESEND_EMAIL = 100;
+}
--- app/Util/Messages.php
+++ app/Util/Messages.php
@@ -1,24 +1,34 @@
 <?php
 
-namespace App\Http\Util;
+namespace App\Util;
 
 /**
  * Class Messages
  *
  * @author JorgeCoronelG
- * @package App\Http\Util
  * @version 1.0
+ * @package App\Http\Util
  * Created 24/10/2020
  */
 class Messages
 {
+    // Mensajes generales
     const MODEL_IS_DIRTY = 'Se debe especificar al menos un valor diferente para actualizar.';
+    const CREDENTIALS_INVALID = 'Credenciales inválidas.';
+    const USER_IS_VERIFIED = 'El usuario ya fue verificado.';
+    const USER_NOT_VERIFIED = 'Usuario no verificado.';
+
+    // Mensajes de excepciones
     const AUTHENTICATION_EXCEPTION = 'No autenticado.';
-    const MODEL_NOT_FOUND_EXCEPTION = 'No existe el recurso.';
+    const MODEL_NOT_FOUND_EXCEPTION = 'No existe el registro.';
     const AUTHORIZATION_EXCEPTION = 'No tiene permisos para ejecutar esta acción.';
     const NOT_FOUND_HTTP_EXCEPTION = 'No se encontró la URL especificada.';
     const METHOD_NOT_ALLOWED_HTTP_EXCEPTION = 'Método no válido.';
     const QUERY_EXCEPTION_1451 = 'No se puede eliminar el recurso porque está relacionado con algún otro.';
-    const ERROR_SERVER = 'Ocurrió algo inesperado. Intente mas tarde.';
+    const INTERNAL_SERVER_ERROR = 'Ocurrió algo inesperado. Intente mas tarde.';
     const THROTTLE_REQUESTS_EXCEPTION = 'Muchos intentos realizados.';
+
+    // Mensajes de asunto de correo electrónico
+    const CONFIRM_EMAIL = 'Confirma tu correo electrónico';
+    const EMAIL_UPDATED = 'Confirma tu nuevo correo electrónico';
 }
