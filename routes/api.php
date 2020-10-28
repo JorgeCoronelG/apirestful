d15877722751550ad9892aad76d8bb9c8edd571e
Jorge Coronel González <tprog.jorge.coronel@outlook.com>
2020-10-28 14:04:16 -0600

    Rutas API Se agregan las rutas para la verificación del token, cambiar email y reenviar token de verificación

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,20 @@
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\League\LeagueController;
 use \App\Http\Controllers\User\LoginController;
+use \App\Http\Controllers\User\UserController;
 
 Route::prefix('v1')->group(function () {
-    Route::post('/login', [LoginController::class, 'login']);
+    // Users
+    Route::post('/login', [LoginController::class, 'login'])->name('login');
+    Route::get('/users/verify/{token}', [UserController::class, 'verify'])->name('verify');
 });
 
 Route::middleware('auth:api')->prefix('v1/app')->group(function () {
+    // Users
+    Route::post('/users/{user}/change-email', [UserController::class, 'updateEmail'])->name('update.email');
+    Route::get('/users/{user}/resend', [UserController::class, 'resend'])->name('resend');
+    Route::get('/users/{user}/logout', [LoginController::class, 'logout'])->name('logout');
+
+    // Leagues
     Route::apiResource('leagues',LeagueController::class);
-    Route::get('/users/{user}/logout', [LoginController::class, 'logout']);
 });
